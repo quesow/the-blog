a2259d40c5d26b910ff8042224c49810ca51994d
JESUA BETANCOR ALEMAN <JBETANCOR.0011@GMAIL.COM>
2018-07-07 16:16:44 +0200

    add comments

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Blog application to train basics of Ruby on Rails
 - [x] Scaffold Posts
 - [x] Add fancy notifications with gritter gem
 - [x] Add bootstrap and simple_form gem
-- [] Add RSpec tests
-- [] Add Comments
-- [] Add users with devise (roles: admin, blogger and visitant)
-- [] TBA
+- [X] Add RSpec tests
+- [X] Add Comments
+- [ ] Improve appearance of blog index view (add pagination)
+- [ ] Add users with devise (roles: admin, blogger and visitant)
+- [ ] TBA
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times.each do
+  Post.create(title: Faker::Name.name , body: Faker::ChuckNorris.fact)
+end
+
+Post.all.each do |post|
+  10.times.each do
+    Comment.create(content: Faker::HowIMetYourMother.quote , post: post)
+  end
+end
